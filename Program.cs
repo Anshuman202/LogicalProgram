167a4e00aea0046943ebb7bbbe3badfc1b642725
Anshuman202 <asanshuman2@gmail.com>
2023-01-01 19:50:52 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ReverseNumber.DisplayNumber();
+            FibonacciSeries.DisplaySeries();
+
+        }
+    }
+}
